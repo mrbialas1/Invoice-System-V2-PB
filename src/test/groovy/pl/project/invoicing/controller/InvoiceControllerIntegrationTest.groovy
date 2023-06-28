a73e3da03c248f4ddd742e6e1a94f31d0c94edc2
mrbialas1 <mrbialas123@gmail.com>
2023-06-28 20:24:04 +0200

    Tests

---
+++ src/test/groovy/pl/project/invoicing/controller/InvoiceControllerIntegrationTest.groovy
@@ -0,0 +1,12 @@
+package pl.project.invoicing.controller
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+import spock.lang.Unroll
+
+@AutoConfigureMockMvc
+@SpringBootTest
+@Unroll
+class InvoiceControllerIntegrationTest extends Specification{
+}
