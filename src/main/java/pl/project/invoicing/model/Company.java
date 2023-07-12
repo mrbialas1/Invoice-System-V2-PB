5f57cc21330edddc4fd52842c461bf52236ac492
mrbialas1 <93154790+mrbialas1@users.noreply.github.com>
2023-07-12 23:09:35 +0200

    Task 8 (#6)
    * API
    
    * Checkstyles

--- src/main/java/pl/project/invoicing/model/Company.java
+++ src/main/java/pl/project/invoicing/model/Company.java
@@ -1,5 +1,6 @@
 package pl.project.invoicing.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,8 +8,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Company {
 
+  @ApiModelProperty(value = "Tax identification number", required = true, example = "798-673-18-66")
   private String taxIdentificationNumber;
+
+  @ApiModelProperty(value = "Company address", required = true, example = "ul. Kozielska 23, 44-100 Gliwice")
   private String address;
+
+  @ApiModelProperty(value = "Company name", required = true, example = "InInvoices")
   private String name;
 
   public Company(String taxIdentificationNumber, String address, String name) {
--- src/main/java/pl/project/invoicing/model/InvoiceEntry.java
+++ src/main/java/pl/project/invoicing/model/InvoiceEntry.java
@@ -1,5 +1,6 @@
 package pl.project.invoicing.model;
 
+import io.swagger.annotations.ApiModelProperty;
 import java.math.BigDecimal;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -10,10 +11,19 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class InvoiceEntry {
 
+  @ApiModelProperty(value = "Product/service description", required = true, example = "GIGABYTE GeForce RTX 3070 Eagle OC LHR 8GB")
   private String description;
+
+  @ApiModelProperty(value = "Number of items", required = true, example = "62")
   private int quantity;
+
+  @ApiModelProperty(value = "Product/service net price", required = true, example = "2211.23")
   private BigDecimal price;
+
+  @ApiModelProperty(value = "Product/service tax value", required = true, example = "176.9")
   private BigDecimal vatValue;
+
+  @ApiModelProperty(value = "Tax rate", required = true)
   private Vat vatRate;
 
 }
