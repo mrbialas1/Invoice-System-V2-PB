771b902f7320fd62972b14dcc012f7d41e1b6d54
mrbialas1 <mrbialas123@gmail.com>
2023-08-10 13:31:18 +0200

    SQL done

--- src/main/java/pl/project/invoicing/InvoiceApplication.java
+++ src/main/java/pl/project/invoicing/InvoiceApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InvoiceApplication {
 
-
   public static void main(String[] args) {
     SpringApplication.run(InvoiceApplication.class, args);
   }
-
 }
