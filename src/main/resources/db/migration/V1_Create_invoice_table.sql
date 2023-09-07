9c6501f9dc07ec6dacc0270956e07018fa90e5ff
mrbialas1 <mrbialas123@gmail.com>
2023-09-07 18:54:17 +0200

    SQL DONE

--- src/main/resources/db/migration/V1_Create_invoice_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE public.invoices
-(
-    id bigint NOT NULL,
-    issue_date date NOT NULL,
-    "number" character varying(50) NOT NULL,
-    PRIMARY KEY (id)
-);
--- src/main/resources/db/migration/V3_alter_invoice_table.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE IF EXIST public.invoices
-    ADD COLUMN seller bigserial NOT NULL:
-
-ALTER TABLE IF EXIST public.invoices
-    ADD COLUMN buyer bigserial NOT NULL:
