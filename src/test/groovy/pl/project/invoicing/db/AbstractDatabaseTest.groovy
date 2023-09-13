d0020a1b5d41dbec18f44fc1a96650f982525ea0
mrbialas1 <mrbialas123@gmail.com>
2023-09-13 19:05:18 +0200

    SQL work

--- src/test/groovy/pl/project/invoicing/db/AbstractDatabaseTest.groovy
+++ src/test/groovy/pl/project/invoicing/db/AbstractDatabaseTest.groovy
@@ -15,7 +15,7 @@ abstract class AbstractDatabaseTest extends Specification {
 
     def "should save invoices returning sequential id, invoice should have id set to correct value, get by id returns saved invoice"() {
         when:
-        def ids = invoices.collect({ database.save(it) })
+        def ids = invoices.collect{it.id = database.save(it) }
 
         then:
         (1..invoices.size() - 1).forEach { assert ids[it] == ids[0] + it }
@@ -33,29 +33,29 @@ abstract class AbstractDatabaseTest extends Specification {
 
     def "get all returns all invoices in the database, deleted invoice is not returned"() {
         given:
-        invoices.forEach({it.id = database.save(it)})
+        invoices.forEach { it.id = database.save(it) }
 
         expect:
         database.getAll().size() == invoices.size()
-        database.getAll().forEach({ assert it == invoices.get(it.getId() - 1) })
+        database.getAll().forEach { assert resetIds(it) == invoices.get(it.getId() - 1) }
 
         when:
         database.delete(1)
 
         then:
         database.getAll().size() == invoices.size() - 1
-        database.getAll().forEach({ assert it == invoices.get(it.getId() - 1) })
-        database.getAll().forEach({ assert it.getId() != 1 })
+        database.getAll().forEach { assert resetIds(it) == invoices.get(it.getId() - 1) }
+        database.getAll().forEach { assert it.getId() != 1 }
 
     }
 
     def "can delete all invoices"() {
         given:
-        database.getAll().isEmpty()
-        //invoices.forEach({ database.save(it) })
+        //database.getAll().isEmpty()
+        invoices.forEach{it.id = database.save(it) }
 
         when:
-        invoices.forEach({ database.delete(it.getId()) })
+        invoices.forEach{database.delete(it.getId()) }
 
         then:
         database.getAll().isEmpty()
@@ -69,21 +69,32 @@ abstract class AbstractDatabaseTest extends Specification {
     def "it's possible to update the invoice, original invoice is returned"() {
         given:
         def originalInvoice = invoices.get(0)
-        int id = database.save(originalInvoice)
+        originalInvoice.id = database.save(originalInvoice)
+
+        def expectedInvoice = invoices.get(1)
+        expectedInvoice.id = originalInvoice.id
 
 
         when:
-        def result = database.update(id, invoices.get(1))
+        def result = database.update(originalInvoice.id, expectedInvoice)
 
 
         then:
-        database.getById(id).get() == invoices.get(1)
-        result == Optional.of(originalInvoice)
+        def invoiceAfterUpdate = database.getById(originalInvoice.id).get()
+        resetIds(invoiceAfterUpdate) == expectedInvoice
+        resetIds(result.get()) == originalInvoice
     }
 
     def "updating not existing invoice returns Optional.empty()"() {
         expect:
         database.update(213, invoices.get(1)) == Optional.empty()
+    }
+
+    // resetting is necessary because database query returns ids while we don't know ids in original invoice
+    private static Invoice resetIds(Invoice invoice) {
+        invoice.getBuyer().id = 0
+        invoice.getSeller().id = 0
+        invoice
     }
 
 }
